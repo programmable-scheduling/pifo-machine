d04455bb01f6ef34800ee47a66f4a6d04db23d11
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-12-23 22:15:30 -0500

    Start testing: strict priority scheduling where all enqs happen before deqs

--- pifo_machine.cc
+++ pifo_machine.cc
@@ -6,27 +6,42 @@
 
 int main() {
   try {
-    // Random priority generation
+    // Random number generation
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<uint32_t> ele_dis(1, 5);
+    std::uniform_int_distribution<int> ele_dis(1, 5);
+
+    // Traffic generation
+    PIFOPacket test_packet;
 
     // Single PIFO pipeline stage consisting of
-    // 1 priority and 0 calendar queues
+    // 1 priority and queue
+    // (every PIFO pipeline stage has 1 calendar queue)
     PIFOPipeline pipeline({PIFOPipelineStage(1,
                                              "fid",
-                                             {{0, {Operation::TRANSMIT, {{0, QueueType::PRIORITY_QUEUE, 0}}}}},
+                                             {{1, {Operation::TRANSMIT, {}}},
+                                              {2, {Operation::TRANSMIT, {}}},
+                                              {3, {Operation::TRANSMIT, {}}},
+                                              {4, {Operation::TRANSMIT, {}}},
+                                              {5, {Operation::TRANSMIT, {}}},
+                                             },
                                              [] (const auto & x) { return x("fid"); })});
 
     // Execute simulation
     for (uint32_t i = 0; i < 10000; i++) {
-      pipeline.enq(0, QueueType::PRIORITY_QUEUE, 0, PIFOPacket(), i);
-      std::cout << pipeline << std::endl;
-      if (i % 5 == 0) {
-        auto result = pipeline.deq(0, QueueType::PRIORITY_QUEUE, 0, i);
-        std::cout << "Deq result is \"" << result << "\"" << std::endl;
-      }
+      test_packet("fid") = ele_dis(gen);
+      test_packet("ptr") = 666;
+      pipeline.enq(0, QueueType::PRIORITY_QUEUE, 0, test_packet, i);
     }
+
+    std::cout << "Finished enqs\n";
+
+    for (uint32_t i = 10000; i < 20000; i++) {
+      auto result = pipeline.deq(0, QueueType::PRIORITY_QUEUE, 0, i);
+      std::cout << "Deq result is \"" << result << "\"" << std::endl;
+    }
+
+    assert_exception(pipeline.deq(0, QueueType::PRIORITY_QUEUE, 0, 20000).initialized() == false);
   } catch (const std::exception & e) {
     std::cerr << "Caught exception in main " << std::endl << e.what() << std::endl;
     return EXIT_FAILURE;
